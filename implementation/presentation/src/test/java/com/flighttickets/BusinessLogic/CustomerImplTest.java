a4c770f9e8c6469fac1db08f2123d46eef6d9fe7
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-03-28 17:48:52 +0200

    CustomerImplTest
    constructor test

--- implementation/presentation/src/test/java/com/flighttickets/BusinessLogic/CustomerImplTest.java
+++ implementation/presentation/src/test/java/com/flighttickets/BusinessLogic/CustomerImplTest.java
@@ -1,7 +1,20 @@
 package com.flighttickets.BusinessLogic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.flighttickets.Entities.Customer;
+import org.junit.jupiter.api.Test;
 
-class CustomerImplTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+public class CustomerImplTest {
+    @Test
+    void constructorTest() {
+        String firstName = "FName";
+        String lastName = "LName";
+        String email = "mail";
+        String password = "pass";
+        String address = "adr";
+        Customer customer = new CustomerImpl(firstName, lastName, email, password, address);
+        assertThat(customer).isExactlyInstanceOf(CustomerImpl.class);
+    }
 }
