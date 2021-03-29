0b4e49753b4a955fc73efcc403bffa8c559751dc
Jasper Lamers <48760178+Schnoipah@users.noreply.github.com>
2021-03-29 13:00:59 +0200

    Added some Login implementation/With todos for next steps

--- implementation/presentation/src/main/java/com/flighttickets/GUI/LoginController.java
+++ implementation/presentation/src/main/java/com/flighttickets/GUI/LoginController.java
@@ -1,6 +1,7 @@
 package com.flighttickets.GUI;
 
 import com.flighttickets.App;
+import com.flighttickets.BusinessLogic.CustomerImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -22,13 +23,24 @@ public class LoginController {
 
     @FXML
     private TextField passwordTbx;
+    //TODO Type needs changing to password - jl
 
     @FXML
     private Button loginBtn;
 
     @FXML
     void handleLogin(ActionEvent event) throws IOException {
-        App.setRoot("salesOfficer");
+        String user = usernameTbx.getText();
+        String passw = passwordTbx.getText();
+        if(user.equals("admin") && passw.equals("admin")){
+            App.setRoot("salesOfficer");
+        } else if(user.equals("customer") && passw.equals("customer")) {
+            //TODO create customer main menu - jl
+            App.setRoot("editTicket");
+        } else {
+            //TODO Implement wrong username error thrown in fxml - jl
+            App.setRoot("login");
+        }
     }
 
 }
