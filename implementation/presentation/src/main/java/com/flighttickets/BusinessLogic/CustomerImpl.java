3840ae28556f1aad8fa00cd535baf4f081127aea
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-01 18:01:17 +0200

    Update CustomerImpl.java
    getters and setters generated

--- implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/CustomerImpl.java
+++ implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/CustomerImpl.java
@@ -12,8 +12,10 @@ public class CustomerImpl implements Customer {
     private String password;
     private String address;
 
+
     /**
      * create customer object
+     *
      * @param firstName
      * @param lastName
      * @param email
@@ -25,8 +27,47 @@ public class CustomerImpl implements Customer {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-
         this.password = password;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 }
