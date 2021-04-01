88bb6dcbaaa43f682ed2235cf5b6def8ac4bf714
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-01 17:58:41 +0200

    Update CustomerManagerImpl.java

--- implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/CustomerManagerImpl.java
+++ implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/CustomerManagerImpl.java
@@ -2,15 +2,25 @@ package com.flighttickets.BusinessLogic;
 
 import com.flighttickets.Entities.Customer;
 import com.flighttickets.Entities.CustomerManager;
+import com.flighttickets.Persistance.CustomerStorageService;
 
 public class CustomerManagerImpl implements CustomerManager {
+    private CustomerStorageService customerStorageService;
+
+
+    public void setCustomerStorageService(CustomerStorageService customerStorageService) {
+        this.customerStorageService = customerStorageService;
+    }
+
+    //TODO: Why not void
     @Override
-    public Customer createCustomer(String firstName, String lastName, String email, String password, String address) {
-        return null;
+    public Customer add(Customer customer) {
+        this.customerStorageService.add(customer);
+        return customer;
     }
 
     @Override
-    public Customer add(Customer customer) {
-        return null;
+    public Customer createCustomer(String firstName, String lastName, String email, String password, String address) {
+        return new CustomerImpl(firstName, lastName, email, password, address);
     }
 }
