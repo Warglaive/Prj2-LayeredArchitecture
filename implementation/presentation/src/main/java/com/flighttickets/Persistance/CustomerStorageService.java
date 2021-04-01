2ed7d41a690d538ec09303c455ddb7a63049d6fd
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-01 17:16:02 +0200

    CustomerStorageService

---
+++ implementation/presentation/src/main/java/com/flighttickets/Persistance/CustomerStorageService.java
@@ -0,0 +1,19 @@
+package com.flighttickets.Persistance;
+
+import com.flighttickets.Entities.Customer;
+
+import java.util.List;
+
+public interface CustomerStorageService {
+    /**
+     * add a Customer object to DB
+     * @param customer
+     */
+    void add(Customer customer);
+
+    /**
+     * get all Customers from DB
+     * @return
+     */
+    List<Customer> getAll();
+}
--- implementation/presentation/src/main/java/com/flighttickets/Persistance/PersistenceAPI.java
+++ implementation/presentation/src/main/java/com/flighttickets/Persistance/PersistenceAPI.java
@@ -1,5 +1,11 @@
 package com.flighttickets.Persistance;
 
+/**
+ * Facade that provides all persistence/storage services to the business logic.
+ *
+ * @author Richard van den Ham {@code r.vandenham@fontys.nl}
+ */
+
 public interface PersistenceAPI {
 
 }
