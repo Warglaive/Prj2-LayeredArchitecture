f1a732e87fb0ce120dd2ab6bebb464fc5e3618c7
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-02 14:46:43 +0200

    BusinessLogicAPIImpl

---
+++ implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/BusinessLogicAPI.java
@@ -0,0 +1,8 @@
+package com.flighttickets.BusinessLogic;
+
+import com.flighttickets.Entities.CustomerManager;
+
+public interface BusinessLogicAPI {
+    CustomerManager getCustomerManager();
+
+}
---
+++ implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/BusinessLogicAPIImpl.java
@@ -0,0 +1,20 @@
+package com.flighttickets.BusinessLogic;
+
+import com.flighttickets.Entities.CustomerManager;
+import com.flighttickets.Persistance.PersistenceAPI;
+
+public class BusinessLogicAPIImpl implements BusinessLogicAPI {
+    
+    final PersistenceAPI persistenceAPI;
+
+    public BusinessLogicAPIImpl(PersistenceAPI persistenceAPI) {
+        this.persistenceAPI = persistenceAPI;
+    }
+
+    @Override
+    public CustomerManager getCustomerManager() {
+        CustomerManagerImpl customerManager = new CustomerManagerImpl();
+        customerManager.setCustomerStorageService(this.persistenceAPI.getCustomerStorageService(customerManager));
+        return customerManager;
+    }
+}
