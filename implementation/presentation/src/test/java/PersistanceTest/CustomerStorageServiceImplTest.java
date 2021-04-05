67747163ff7f3bb763c82ad6230bc204236a1dbf
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-05 21:57:14 +0200

    todos
    added todos

---
+++ implementation/presentation/src/test/java/PersistanceTest/CustomerStorageServiceImplTest.java
@@ -0,0 +1,14 @@
+package PersistanceTest;
+import com.flighttickets.Entities.Customer;
+import com.flighttickets.Entities.CustomerManager;
+import com.flighttickets.Persistance.CustomerStorageService;
+import com.flighttickets.Persistance.CustomerStorageServiceImpl;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class CustomerStorageServiceImplTest {
+    @Test
+    void
+}
