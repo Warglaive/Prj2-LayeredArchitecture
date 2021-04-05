500af3647a2a12dbee69e4ee2eb306b05b2423a1
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-05 22:01:19 +0200

    Update CustomerStorageServiceImplTest.java

--- implementation/presentation/src/test/java/PersistanceTest/CustomerStorageServiceImplTest.java
+++ implementation/presentation/src/test/java/PersistanceTest/CustomerStorageServiceImplTest.java
@@ -1,4 +1,7 @@
 package PersistanceTest;
+
+import com.flighttickets.BusinessLogic.CustomerImpl;
+import com.flighttickets.BusinessLogic.CustomerManagerImpl;
 import com.flighttickets.Entities.Customer;
 import com.flighttickets.Entities.CustomerManager;
 import com.flighttickets.Persistance.CustomerStorageService;
@@ -8,7 +11,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+
 public class CustomerStorageServiceImplTest {
+    CustomerStorageServiceImpl storageService;
+
+    @BeforeEach
+    void setUp() {
+        CustomerManager customerManager = new CustomerManagerImpl();
+        this.storageService = new CustomerStorageServiceImpl(customerManager);
+    }
+
     @Test
-    void
+    void constructorTest() {
+        assertThat(this.storageService).isExactlyInstanceOf(CustomerStorageServiceImpl.class);
+    }
+
+    @Test
+    void addTest() throws ClassNotFoundException {
+        String firstName = "1";
+        String lastName = "1";
+        String email = "1";
+        String password = "1";
+        String address = "1";
+        int level = 2;
+        //create new customer
+        Customer actual = new CustomerImpl(firstName, lastName, email, password, address, level);
+        //add customer to Storage
+        this.storageService.add(actual);
+        //get customer
+        Customer expected = this.storageService.getByEmail(email);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
+    }
 }
