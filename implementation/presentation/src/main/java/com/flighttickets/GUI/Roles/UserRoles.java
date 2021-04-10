a9d99cc3656373a8010719822922990964bdaefd
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-10 15:22:52 +0200

    Create UserRoles.java

---
+++ implementation/presentation/src/main/java/com/flighttickets/GUI/Roles/UserRoles.java
@@ -0,0 +1,11 @@
+package com.flighttickets.GUI.Roles;
+
+/**
+ * Roles to be used by the register.fxml and login.fxml
+ */
+public enum UserRoles {
+    CustomerRole,
+    SalesOfficerRole,
+    SalesEmployee,
+    Planner
+}
