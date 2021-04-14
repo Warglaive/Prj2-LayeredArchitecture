ac5d86e3328cad3aaa159c619745b1803808e345
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-14 11:41:32 +0200

    Update DAO.java

--- implementation/presentation/src/main/java/com/flighttickets/Entities/DAO.java
+++ implementation/presentation/src/main/java/com/flighttickets/Entities/DAO.java
@@ -5,13 +5,33 @@ import javafx.collections.ObservableList;
 import java.sql.ResultSet;
 
 public interface DAO<T> {
+    /**
+     * @param id
+     * @return resultSet of objects with corresponding ID received from DB
+     */
     T getById(long id);
 
+    /**
+     * @param resultSet - received from DB
+     * @return make resultSet to actual object to work with
+     */
     T getEntityFromResultSet(ResultSet resultSet);
 
+    /**
+     * @return resultSet of objects
+     */
     ObservableList<T> getAll();
 
+
+    /**
+     * @param resultSet - received from DB
+     * @return ObservableList of Objects
+     */
     ObservableList<T> getAllFromResultSet(ResultSet resultSet);
 
-    
+    /**
+     * @param email - new email
+     * @param id    of object(can be Customer/SalesOfficer etc.)
+     */
+    void updateEmail(String email, int id);
 }
