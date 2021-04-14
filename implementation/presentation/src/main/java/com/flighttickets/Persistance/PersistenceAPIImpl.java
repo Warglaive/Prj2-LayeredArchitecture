197996e93966b8f77b6665ff5a703bdec4967630
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-14 16:52:14 +0200

    Update PersistenceAPIImpl.java

--- implementation/presentation/src/main/java/com/flighttickets/Persistance/PersistenceAPIImpl.java
+++ implementation/presentation/src/main/java/com/flighttickets/Persistance/PersistenceAPIImpl.java
@@ -1,10 +1,8 @@
 package com.flighttickets.Persistance;
 
-import com.flighttickets.Entities.CustomerManager;
-
 public class PersistenceAPIImpl implements PersistenceAPI {
     @Override
     public CustomerStorageService getCustomerStorageService() {
-        return new CustomerStorageServiceImpl();
+        return new CustomerStorageService();
     }
 }
