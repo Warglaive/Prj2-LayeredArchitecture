a48f070a9b935853da5e3da6f4da3e1787d90466
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-15 17:46:29 +0200

    Update Customer.java
    getters and setters

--- implementation/presentation/src/main/java/com/flighttickets/Entities/Customer.java
+++ implementation/presentation/src/main/java/com/flighttickets/Entities/Customer.java
@@ -70,4 +70,58 @@ public class Customer implements Entity2<Integer> {
                 this.role
         };
     }
+    /**
+     * Getters and setters for each field
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
