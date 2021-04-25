3908b37309bafce03cf773b8c94391796fc792c0
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-25 19:58:36 +0200

    ClassDiagram

--- implementation/presentation/src/main/java/com/flighttickets/Entities/BookingRequest.java
+++ implementation/presentation/src/main/java/com/flighttickets/Entities/BookingRequest.java
@@ -1,15 +1,23 @@
 package com.flighttickets.Entities;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import nl.fontys.sebivenlo.dao.Entity2;
 import nl.fontys.sebivenlo.dao.ID;
 
 public class BookingRequest implements Entity2<Integer> {
-@ID
-   private Integer bookingId;
+    @ID
+    private Integer id;
 
+    @FXML
+    private DatePicker departureDate;
+
+    BookingRequest(){
+     this.departureDate.getValue();
+    }
     @Override
     public Integer getNaturalId() {
-        return null;
+        return this.id;
     }
 
     @Override
