9cddc711b0d407393086c8092d81a3b6e18483a8
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-04-25 21:33:58 +0200

    Update BookingRequest.java

--- implementation/presentation/src/main/java/com/flighttickets/Entities/BookingRequest.java
+++ implementation/presentation/src/main/java/com/flighttickets/Entities/BookingRequest.java
@@ -17,12 +17,26 @@ public class BookingRequest implements Entity2<Integer> {
     /**
      * Only SystemUser with role == "Customer" can access this class.
      */
+    //TODO: Maybe change to int and customer(id)
     private SystemUser customer;
     /**
      * SalesOfficer to be set by SalesOfficer entity when action is performed(Accept/Reject)
      */
+    //TODO: Maybe change to int and salesOfficer(id)
     private SystemUser salesOfficer;
+    private int passengersAmount;
+    private boolean status;
 
+    public BookingRequest(int id, LocalDate departureDate, LocalDate returnDate, String departureDestination, String destination, SystemUser customer, int passengersAmount, boolean status) {
+        this.id = id;
+        this.departureDate = departureDate;
+        this.returnDate = returnDate;
+        this.departureDestination = departureDestination;
+        this.destination = destination;
+        this.customer = customer;
+        this.passengersAmount = passengersAmount;
+        this.status = status;
+    }
 
     @Override
     public Integer getNaturalId() {
@@ -32,5 +46,49 @@ public class BookingRequest implements Entity2<Integer> {
     @Override
     public int getId() {
         return 0;
+    }
+
+    /**
+     * needed for the Mapper
+     *
+     * @return
+     */
+    //TODO: Check customer.ToString() - may cause issues
+    @Override
+    public String toString() {
+        return "BookingRequest{" + "id=" + id + ", departureDate=" + departureDate + ", returnDate=" + returnDate + ", departureDestination="
+                + departureDestination + ", destination=" + destination + ", customer=" + customer.toString()
+                + ", passengersAmount=" + passengersAmount
+                + ", status=" + status + '}';
+    }
+
+    /**
+     * needed for the Mapper
+     *
+     * @param parts
+     * @return
+     */
+    static BookingRequest fromParts(Object[] parts) {
+        return new BookingRequest((Integer) parts[0], (LocalDate) parts[1], (LocalDate) parts[2], (String) parts[3], (String) parts[4], (SystemUser) parts[5],
+                (Integer) parts[6], (boolean) parts[7]);
+    }
+
+    /**
+     * needed for the Mapper
+     *
+     * @return
+     */
+    Object[] asParts() {
+
+        return new Object[]{
+                this.id,
+                this.departureDate,
+                this.returnDate,
+                this.departureDestination,
+                this.destination,
+                this.customer,
+                this.passengersAmount,
+                this.status
+        };
     }
 }
