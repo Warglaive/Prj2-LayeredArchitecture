1b33ae5c895f48e49b52090c0ddbc7c8e3826cb7
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-05-22 19:10:14 +0200

    AllBookingRequestsController

---
+++ implementation/presentation/src/main/java/com/flighttickets/GUI/AllBookingRequestsController.java
@@ -0,0 +1,13 @@
+package com.flighttickets.GUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AllBookingRequestsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
