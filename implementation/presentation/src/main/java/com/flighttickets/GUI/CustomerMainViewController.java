264b498240a52e1ebc9eab0c8995fa1059510b10
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-05-22 18:39:34 +0200

    Update CustomerMainViewController.java

--- implementation/presentation/src/main/java/com/flighttickets/GUI/CustomerMainViewController.java
+++ implementation/presentation/src/main/java/com/flighttickets/GUI/CustomerMainViewController.java
@@ -2,7 +2,11 @@ package com.flighttickets.GUI;
 
 import com.flighttickets.Entities.BookingRequestManager;
 import com.flighttickets.Entities.SystemUser;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +17,14 @@ public class CustomerMainViewController implements Initializable {
     private final Supplier<SceneManager> sceneManagerSupplier;
     private final SystemUser loggedInCustomer;
 
+
+    @FXML
+    private TitledPane titledPane;
+
+    @FXML
+    private Button newBookingRequestBtn;
+
+
     public CustomerMainViewController(Supplier<SceneManager> sceneManagerSupplier, SystemUser loggedInCustomer, BookingRequestManager bookingRequestManager) {
         this.sceneManagerSupplier = sceneManagerSupplier;
         this.loggedInCustomer = loggedInCustomer;
@@ -21,5 +33,10 @@ public class CustomerMainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    void viewBookingRequestCreation() {
+        this.sceneManagerSupplier.get().changeScene("CreateBookingRequest");
     }
 }
