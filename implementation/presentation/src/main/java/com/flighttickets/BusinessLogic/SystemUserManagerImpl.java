1d70c935de73443bbbe012234abbf9e84675b206
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-06-01 14:57:16 +0200

    Update SystemUserManagerImpl.java

--- implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/SystemUserManagerImpl.java
+++ implementation/presentation/src/main/java/com/flighttickets/BusinessLogic/SystemUserManagerImpl.java
@@ -45,50 +45,6 @@ public class SystemUserManagerImpl implements SystemUserManager {
      */
     @Override
     public SystemUser createSystemUser(int id, String firstName, String lastName, String email, String password, String address, String role) {
-      /*  //TODO: Validate each property of SystemUser and throw CUSTOM exception if something is wrong.
-        //TODO: Make a array of boolean and throw exception for each isValid == false
-
-        boolean isFirstNameValid = this.inputValidator.isNameValid(firstName);
-        boolean isLastNameValid = this.inputValidator.isNameValid(lastName);
-        boolean isEmailValid = this.inputValidator.isEmailValid(email);
-        boolean isPasswordValid = this.inputValidator.isPasswordValid(password);
-        boolean isAddressValid = this.inputValidator.isAddressValid(address);
-        boolean isRoleValid = this.inputValidator.isRoleValid(role);
-        boolean isAnyInvalid = false;
-        //Add to string exception msg for each of the invalid input arguments
-        String exceptionMessage = "";
-        if (!isFirstNameValid) {
-            exceptionMessage += "Invalid first name!";
-            isAnyInvalid = true;
-        }
-
-        if (!isLastNameValid) {
-            exceptionMessage = "Invalid last name!";
-            isAnyInvalid = true;
-        }
-
-        if (!isEmailValid) {
-            exceptionMessage = "Invalid email address!";
-            isAnyInvalid = true;
-        }
-
-        if (!isPasswordValid) {
-            exceptionMessage = "Invalid password!";
-            isAnyInvalid = true;
-        }
-
-        if (!isAddressValid) {
-            exceptionMessage = "Invalid address!";
-            isAnyInvalid = true;
-        }
-
-        if (!isRoleValid) {
-            exceptionMessage = "Invalid role!";
-            isAnyInvalid = true;
-        }
-        if (isAnyInvalid) {
-            throw new InvalidInputException(exceptionMessage);
-        }*/
         return new SystemUser(id, firstName, lastName, email, password, address, role);
     }
 
